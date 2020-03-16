6925a8183268f6b15b10ede02efec84ae4bdee5f
Alejandro Roa <alejodev04@gmail.com>
2020-03-16 18:11:35 -0400

    add styles with material UI

--- src/components/CountryCasesList.jsx
+++ src/components/CountryCasesList.jsx
@@ -2,36 +2,73 @@ import React, { Component } from 'react'
 import Fragment from 'render-fragment';
 
 // MAERIAL UI
+import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+
+
+const container = {
+    padding: '40px'
+};
+const card = {
+    border: 0,
+    borderRadius: '16px',
+    padding: '40px',
+};
+const textConfirmed = {
+    fontSize: '2em',
+    color: '#4791db'
+}
+const textRecovered = {
+    fontSize: '2em',
+    color: '#4caf50'
+}
+const textDeath = {
+    fontSize: '2em',
+    color: '#f44336'
+}
+const divider = {
+    marginTop: '20px'
+}
 
 export default class CountryCasesList extends Component {
     render() {
         const { confirmed, recovered, deaths } = this.props
         return (
             <Fragment>
-                 <Card>
-                    <CardContent>
-                        <Typography>
-                            Confirmed: {confirmed}
-                        </Typography>
-                    </CardContent>
-                </Card>
-                <Card>
-                    <CardContent>
-                        <Typography>
-                            Recovered: {recovered}
-                        </Typography>
-                    </CardContent>
-                </Card>
-                <Card>
-                    <CardContent>
-                        <Typography>
-                            Deaths: {deaths}
-                        </Typography>
-                    </CardContent>
-                </Card>
+                <Divider style={divider} />
+                <Grid container spacing={1} style={container}>
+                    <Grid item sm={4} xs={12}>
+                        <Card style={card}>
+                            <CardContent>
+                                <Typography style={textConfirmed}>
+                                    Confirmed: {confirmed}
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                    <Grid item sm={4} xs={12}>
+                        <Card style={card}>
+                            <CardContent>
+                                <Typography style={textRecovered}>
+                                    Recovered: {recovered}
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                    <Grid item sm={4} xs={12}>
+                        <Card style={card}>
+                            <CardContent>
+                                <Typography style={textDeath}>
+                                    Deaths: {deaths}
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                </Grid>
+                <Divider style={divider} />
             </Fragment>
         )
     }
--- src/components/HeaderForm.jsx
+++ src/components/HeaderForm.jsx
@@ -2,29 +2,36 @@ import React from 'react'
 import { reduxForm, Field, formValueSelector } from 'redux-form'
 import { connect } from 'react-redux'
 
-let HeaderForm = props => {
-    const {
-      handleSubmit,
-    } = props;
-    return (
-      <form onSubmit={handleSubmit}>
-        <Field name='country' component='input' placeholder='COUNTRY' />
-      </form>
-    );
-  };
-  
-  HeaderForm = reduxForm({
-    form: 'country', 
-  })(HeaderForm);
+const input = {
+  fontSize: '20px',
+  padding: '18.5px 14px',
+  height: '1.1875em',
+  borderTopLeftRadius: '4px',
+  borderTopRightRadius: '4px'
+}
 
-  const selector = formValueSelector('country');
-  HeaderForm = connect(state => {
-    const country = selector(state, 'country');
-    localStorage.setItem('country', country);
-    return {
-      country
-    };
-  })(HeaderForm);
-  
-  export default HeaderForm;
-  
+let HeaderForm = props => {
+  const {
+    handleSubmit,
+  } = props;
+  return (
+    <form onSubmit={handleSubmit}>
+      <Field name='country' component='input' placeholder='COUNTRY' style={input} />
+    </form>
+  );
+};
+
+HeaderForm = reduxForm({
+  form: 'country',
+})(HeaderForm);
+
+const selector = formValueSelector('country');
+HeaderForm = connect(state => {
+  const country = selector(state, 'country');
+  localStorage.setItem('country', country);
+  return {
+    country
+  };
+})(HeaderForm);
+
+export default HeaderForm;
