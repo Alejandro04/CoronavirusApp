3ce8f2bfaa842fef8a92ab48a5f5472da5b9c529
Alejandro Roa <alejodev04@gmail.com>
2020-03-16 18:30:08 -0400

    add styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,19 @@ import React, { Component }  from 'react';
 import HeaderForm from './HeaderForm'
 import Fragment from 'render-fragment';
 
+const title = {
+    marginTop: '60px',
+    color: 'rgb(25, 118, 210)'
+}
+
 export default class Header extends Component {
     render() {
         const { setCountry } = this.props
         return (
             <Fragment>
-                <h3>Country - <span>{localStorage.getItem('country')}</span></h3>
+                <h3>Write the country in English / Escriba el pais en ingles</h3>
                 <HeaderForm onSubmit={setCountry}/>
+                <h2 style={title}>Country - <span>{localStorage.getItem('country')}</span></h2>
             </Fragment>
         )
     }
