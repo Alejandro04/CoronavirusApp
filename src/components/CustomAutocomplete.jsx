320b13f857077ff153241708727f512002d2d7df
Alejandro Roa <alejandro@groowly.com>
2020-03-18 20:35:34 -0400

    add select

--- src/components/CustomAutocomplete.jsx
+++ src/components/CustomAutocomplete.jsx
@@ -3,13 +3,15 @@ import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 
 export default function CustomAutocomplete() {
+
   return (
     <Autocomplete
-      id="country"
+    //  onChange={(event, value) => localStorage.setItem('country', value)}
       options={countries}
       getOptionLabel={option => option.title}
       style={{ width: 300 }}
-      renderInput={params => <TextField {...params} label="Country / Pais" variant="outlined" name="country" />}
+      renderInput={params => <TextField {...params} 
+      label="Country / Pais" variant="outlined"  />}
     />
   );
 }
--- src/components/HeaderForm.jsx
+++ src/components/HeaderForm.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
 import { reduxForm, Field, formValueSelector } from 'redux-form'
-import CustomAutocomplete from './CustomAutocomplete'
 import { connect } from 'react-redux'
+
+//MATERIAL UI
+import Button from '@material-ui/core/Button';
+
+const select = {
+  width: '30%',
+  padding: '10px',
+  borderRadius: '10px',
+  margin: 'auto',
+  cursor: 'pointer',
+  marginRight: '15px'
+};
 
 let HeaderForm = props => {
   const {
@@ -9,8 +20,28 @@ let HeaderForm = props => {
   } = props;
   return (
     <form onSubmit={handleSubmit}>
-       {/*<Field name='country' component='input' placeholder='COUNTRY' />*/}
-       <Field component={CustomAutocomplete} />
+      {/*<Field name='country' component='input' placeholder='COUNTRY' />*/}
+      {/*<Field component={CustomAutocomplete}  name="country"/>*/}
+      <Field name="country" component="select" style={select}>
+        <option value="">Country / País</option>
+        <option value="venezuela">Venezuela</option>
+        <option value="colombia">Colombia</option>
+        <option value="brazil">Brasil</option>
+        <option value="us">Estados Unidos</option>
+        <option value="canada">Canadá</option>
+        <option value="peru">Peru</option>
+        <option value="argentina">Argentina</option>
+        <option value="ecuador">Ecuador</option>
+        <option value="bolivia">Bolivia</option>
+        <option value="mexico">México</option>
+        <option value="spain">España</option>
+        <option value="italy">Italia</option>
+        <option value="china">China</option>
+        <option value="uruguay">Uruguay</option>
+        <option value="paraguay">Paraguay</option>
+        <option value="germany">Alemania</option>
+      </Field>
+      <Button variant="contained" color="primary" type="submit">Buscar</Button>
     </form>
   );
 };
