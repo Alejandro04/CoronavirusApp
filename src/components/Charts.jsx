d7a3a5d4abdad5af931c3f88c1019da79ecfe00c
Alejandro Roa <alejandro@groowly.com>
2020-03-22 09:03:48 -0400

    wip

--- src/components/Charts.jsx
+++ src/components/Charts.jsx
@@ -1,14 +1,53 @@
-  
-import React, { Component }  from 'react';
+
+import React, { PureComponent } from 'react';
+import {
+    LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
+} from 'recharts';
 import Fragment from 'render-fragment';
 
-export default class Charts extends Component {
+const data = [
+    {
+        name: 'Dia 1', confirmed: 2, recovered: 0, death: 0,
+    },
+    {
+        name: 'Dia 2', confirmed: 8, recovered: 1, death: 0,
+    },
+    {
+        name: 'Dia 3', confirmed: 15, recovered: 5, death: 0,
+    },
+    {
+        name: 'Dia 4', confirmed: 20, recovered: 8, death: 0,
+    }
+];
+
+const chartStyle = {
+    margin: 'auto'
+}
+
+export default class Charts extends PureComponent {
+
     render() {
-        const { chartsConfirmed, chartsRecovered, chartsDeath } = this.props
         return (
             <Fragment>
-                Charts2
+                <LineChart
+                    width={500}
+                    height={300}
+                    data={data}
+                    margin={{
+                        top: 5, right: 30, left: 20, bottom: 5,
+                    }}
+                    style={chartStyle}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Line type="monotone" dataKey="confirmed" stroke="#8884d8" activeDot={{ r: 8 }} />
+                    <Line type="monotone" dataKey="recovered" stroke="#82ca9d" />
+                    <Line type="monotone" dataKey="death" stroke="red" />
+                </LineChart>
             </Fragment>
-        )
+        );
     }
 }
